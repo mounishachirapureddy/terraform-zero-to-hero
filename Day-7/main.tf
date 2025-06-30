07b846530be3b2cb34f29ba4b99e5ea501c33bcc
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2025-06-30 11:44:19 +0530

    Update main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "http://56.228.41.226:8200/"
-  skip_child_token = true
+  address           = "http://56.228.41.226:8200/"
+  skip_child_token  = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<b688f2b6-ba4b-ea62-2f85-cd5a00538c65>"
-      secret_id = "<c1ab4fc0-672a-e693-3550-875e1f2af15a>"
+      role_id   = "b688f2b6-ba4b-ea62-2f85-cd5a00538c65"
+      secret_id = "c1ab4fc0-672a-e693-3550-875e1f2af15a"
     }
   }
 }
 
-data "vault_kkv2_secret" "example" {
-  mount = "secret" // change it according to your mount
-  name  = "test-secret" // change it according to your secret
+data "vault_kv_secret_v2" "example" {
+  mount = "secret"       # this is the path enabled for KV
+  name  = "test-secret"  # this is the secret key you created
 }
 
 resource "aws_instance" "my_instance" {
@@ -26,7 +26,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kkv2_secret.example.data["foo"]
+    Name   = "vault-secret-test"
+    Secret = data.vault_kv_secret_v2.example.data["foo"]
   }
 }
